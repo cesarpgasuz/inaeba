b61cae4705b69def238106747702a5c6bb296dc5
Cesar Pablo Garcia Suarez <cesarpgasuz@gmail.com>
2023-11-29 14:48:26 -0600

    Update Prospectos.jsx

--- components/Prospectos.jsx
+++ components/Prospectos.jsx
@@ -24,10 +24,9 @@ function Prospectos() {
         <p className="mt-5 mb-10">Si realizaste una cita para el dia 28 de noviembre, Deberas presentarte en nuestras oficionas ubicadas en Calle León, S/N, Centro, Doctor Mora. en un <strong>Horario de 10:00 am a 3:00pm</strong></p>
         <Image src={ImagenOficina} width={900} height={550} alt="Oficina Inaeba" className="mb-10"/>
         <Image src={ImagenOficina2} width={900} height={550} alt="Oficina Inaeba" className="mb-10"/>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d2237.2991999861897!2d-100.32559056270281!3d21.143041832841714!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1ses-419!2smx!4v1701265964914!5m2!1ses-419!2smx" 
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d665.1527570501308!2d-100.32567650905008!3d21.14310301925747!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d4bf667ae2aa69%3A0x80790d2734b80af6!2sInaeba%20Doctor%20Mora!5e1!3m2!1ses-419!2smx!4v1701289570976!5m2!1ses-419!2smx" 
         className="w-full h-96"
-        allowfullscreen="" 
-        loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        loading="lazy"></iframe>
 
     </section>
   )
